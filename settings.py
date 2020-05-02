81ad5ca90a0cd2c57ff6edaf4e4385b04136ca90
Chris Matian <chris.matian@race.com>
2020-05-01 18:16:51 -0700

    Working on Game options next

--- settings.py
+++ settings.py
@@ -1,5 +1,22 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        # Alien
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1  # 1 = right, -1 = left
+
+        # Screen
+        self.screen_width = 1100
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
+
+        # Ship
+        self.ship_speed = 1.5
+        self.ship_limit = 3
+
+        # Bullet
+        self.bullet_speed = 1.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
